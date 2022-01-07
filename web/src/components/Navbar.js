d8299f54f504383aa8a082f4b54dcdd4570d02be
Juan Pablo Cano <Canojuan0415@gmail.com>
2022-01-07 11:00:42 -0500

    Cambios en el CSS

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../images/logo.png"
 
 export const PublicNavbar = () => (
   <nav>
     <section>
       <Link to="/">Home</Link>
+      <img className='navbar-logo' src={logo} alt='...'/>
       <Link to="/questions">Questions</Link>
+      <Link className='navbar-login__button' to={"/login"}>Log-in</Link>
+      <Link className='navbar-login__button' to={"/login"}>Log-out</Link>
     </section>
   </nav>
 )
@@ -14,6 +18,7 @@ export const PrivateNavbar = () => (
   <nav>
     <section>
       <Link to="/">Home</Link>
+      <img className='navbar-logo' src={logo} alt='...'/>
       <Link to="/questions">Questions</Link>
       <Link to="/new">New</Link>
       <Link to="/list">List</Link>
