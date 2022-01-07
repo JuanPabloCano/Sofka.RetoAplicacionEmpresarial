7fece3e78a5b85ed8f69306ca9f327d36013cbbb
Juan Pablo Cano <Canojuan0415@gmail.com>
2022-01-06 22:24:12 -0500

    Creación de Footer y Logo de página

--- web/src/App.js
+++ web/src/App.js
@@ -9,9 +9,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import { login, logout } from './actions/authActions';
-
 import { PublicNavbar, PrivateNavbar } from './components/Navbar'
 import HomePage from './pages/HomePage'
+import Footer from "./components/Footer"
 import SingleQuestionPage from './pages/SingleQuestionPage'
 import QuestionsPage from './pages/QuestionsPage'
 import QuestionFormPage from './pages/QuestionFormPage'
@@ -20,55 +20,58 @@ import OwnerQuestionsPage from './pages/OwnerQuestionsPage'
 import { useAuthState } from "react-firebase-hooks/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCTySyvuIDPg7RWF6ceuuwC2t3BEiAK38o",
-  authDomain: "question-app-demo.firebaseapp.com",
-  projectId: "question-app-demo",
-  storageBucket: "question-app-demo.appspot.com",
-  messagingSenderId: "1038673531562",
-  appId: "1:1038673531562:web:da90421f639a3115dcf6d3"
+  apiKey: "AIzaSyBH5JVKTgPqkI9eMDpyQMh11jvfQPmfbP8",
+  authDomain: "aplicacion-empresarial.firebaseapp.com",
+  projectId: "aplicacion-empresarial",
+  storageBucket: "aplicacion-empresarial.appspot.com",
+  messagingSenderId: "295300895767",
+  appId: "1:295300895767:web:c5ddb14829f5d8313e7852",
+  measurementId: "G-ZL8DKTPSYE"
 });
 
 const auth = firebase.auth();
 
 const App = ({ dispatch }) => {
   const [user] = useAuthState(auth);
-  if(user){
+  if (user) {
     dispatch(login(user.email, user.uid))
   }
   return (
-    <Router>
-      {user ?
-        <>
-          <PrivateNavbar />
-          <Switch>
-            <Route exact path="/" component={() => {
-              return <HomePage><SignOut dispatch={dispatch} /></HomePage>
-            }} />
-            <Route exact path="/questions" component={QuestionsPage} />
-            <Route exact path="/question/:id" component={SingleQuestionPage} />
-            <Route exact path="/list" component={OwnerQuestionsPage} />
-            <Route exact path="/answer/:id" component={AnswerFormPage} />
-            <Route exact path="/new" component={QuestionFormPage} />
-            <Redirect to="/" />
-          </Switch>
-        </> :
-        <>
-          <PublicNavbar />
-          <Switch>
-            <Route exact path="/" component={() => {
-              return <HomePage><SignIn dispatch={dispatch} /></HomePage>
-            }} />
-            <Route exact path="/questions" component={QuestionsPage} />
-            <Route exact path="/question/:id" component={SingleQuestionPage} />
-            <Route exact path="/answer/:id" component={AnswerFormPage} />
-            <Redirect to="/" />
-          </Switch>
-        </>
-      }
-    </Router>
+    <>
+      <Router>
+        {user ?
+          <>
+            <PrivateNavbar />
+            <Switch>
+              <Route exact path="/" component={() => {
+                return <HomePage><SignOut dispatch={dispatch} /></HomePage>
+              }} />
+              <Route exact path="/questions" component={QuestionsPage} />
+              <Route exact path="/question/:id" component={SingleQuestionPage} />
+              <Route exact path="/list" component={OwnerQuestionsPage} />
+              <Route exact path="/answer/:id" component={AnswerFormPage} />
+              <Route exact path="/new" component={QuestionFormPage} />
+              <Redirect to="/" />
+            </Switch>
+          </> :
+          <>
+            <PublicNavbar />
+            <Switch>
+              <Route exact path="/" component={() => {
+                return <HomePage><SignIn dispatch={dispatch} /></HomePage>
+              }} />
+              <Route exact path="/questions" component={QuestionsPage} />
+              <Route exact path="/question/:id" component={SingleQuestionPage} />
+              <Route exact path="/answer/:id" component={AnswerFormPage} />
+              <Redirect to="/" />
+            </Switch>
+          </>
+        }
+      </Router>
+      <Footer></Footer>
+    </>
   )
 }
-
 
 function SignIn() {
   const signInWithGoogle = () => {
@@ -94,5 +97,4 @@ function SignOut({ dispatch }) {
   );
 }
 
-
-export default App
+export default App;
---
+++ web/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+
+    <div className="footer">
+        <p>&copy; Sofka Questions And Answers App Game</p>
+        <p>Made By: Juan Pablo Cano Saldarriaga</p>
+    </div>
+
+);
+
+export default Footer;
